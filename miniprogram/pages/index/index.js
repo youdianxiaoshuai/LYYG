b7943580579c818bc1c19123be10d6123bed8b0c
Alex Shao <estsmart@163.com>
2018-10-11 19:03:49 +0800

    主页制作

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -7,7 +7,10 @@ Page({
   data: {
     menu: ["吃的1", "吃的2", "吃的3", "吃的4"],
     inputShowed: false,
-    inputVal: ""
+    inputVal: "",
+    menuItem: [
+      { src: "../../images/meishi.png", name: "美食" }, { src: "../../images/yinpin.png", name: "饮品" }, { src: "../../images/shushi.png", name: "熟食" }, { src: "../../images/dizhi.png", name: "低脂食物" }, { src: "../../images/fenlei.png", name: "分类" }, { src: "../../images/kaluli.png", name: "卡路里排行" }, { src: "../../images/chongzhi.png", name: "充值中心" }, { src: "../../images/linghongbao.png", name: "领红包" }, { src: "../../images/bangmangdai.png", name: "帮忙带" }, { src: "../../images/zhinengtuijian.png", name: "智能推荐" }
+    ]
   },
   showInput: function () {
     this.setData({

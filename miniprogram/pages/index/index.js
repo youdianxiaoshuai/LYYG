aa7ac883c90e8a41f3ba88c2cc7cffaefdda1f34
Alex Shao <estsmart@163.com>
2018-10-13 01:55:50 +0800

    kaluli_Alter

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -9,7 +9,7 @@ Page({
     inputShowed: false,
     inputVal: "",
     menuItem: [
-      { src: "../../images/meishi.png", name: "美食", path: "/pages/canteen/canteen" }, { src: "../../images/yinpin.png", name: "饮品", path: "../canteen/canteen" }, { src: "../../images/shushi.png", name: "熟食", path: "../canteen/canteen" }, { src: "../../images/dizhi.png", name: "低脂食物" }, { src: "../../images/fenlei.png", name: "分类", path: "../canteen/canteen" }, { src: "../../images/kaluli.png", name: "卡路里排行", path: "../canteen/canteen" }, { src: "../../images/chongzhi.png", name: "充值中心", path: "../canteen/canteen" }, { src: "../../images/linghongbao.png", name: "领红包", path: "../canteen/canteen" }, { src: "../../images/bangmangdai.png", name: "帮忙带", path: "../canteen/canteen" }, { src: "../../images/zhinengtuijian.png", name: "智能推荐", path: "../canteen/canteen"}
+      { src: "../../images/meishi.png", name: "美食", path: "/pages/canteen/canteen" }, { src: "../../images/yinpin.png", name: "饮品", path: "../canteen/canteen" }, { src: "../../images/shushi.png", name: "熟食", path: "../canteen/canteen" }, { src: "../../images/dizhi.png", name: "低脂食物" }, { src: "../../images/fenlei.png", name: "分类", path: "../canteen/canteen" }, { src: "../../images/kalulix.png", name: "卡路里排行", path: "../canteen/canteen" }, { src: "../../images/chongzhi.png", name: "充值中心", path: "../canteen/canteen" }, { src: "../../images/linghongbao.png", name: "领红包", path: "../canteen/canteen" }, { src: "../../images/bangmangdai.png", name: "帮忙带", path: "../canteen/canteen" }, { src: "../../images/zhinengtuijian.png", name: "智能推荐", path: "../canteen/canteen"}
     ]
   },
   showInput: function () {
